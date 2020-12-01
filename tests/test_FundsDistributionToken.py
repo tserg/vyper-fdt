54a82b1a8a2e056922663a27fd97e1af687ddbab
tserg <tse.rong.gary@gmail.com>
2020-12-01 19:57:19 +0800

    test: added same IO address

--- tests/test_FundsDistributionToken.py
+++ tests/test_FundsDistributionToken.py
@@ -16,3 +16,16 @@ def test_initial_state(FDT_contract, accounts):
 
 	assert FDT_contract.balanceOf(accounts[0]) == 100
 	assert FDT_contract.balanceOf(accounts[1]) == 0
+
+def test_same_IO(FDT_contract, accounts):
+
+	tx2 = FDT_contract.payToContract({'from': accounts[0], 'amount': 1e18})
+
+	assert FDT_contract.balance() == 1e18
+
+	tx1 = FDT_contract.withdrawFunds({'from': accounts[0]})
+
+	assert len(tx1.events) == 2
+	assert tx1.events['FundsDistributed']['receiver'] == accounts[0]
+	assert tx1.events['FundsWithdrawn']['receiver'] == accounts[0]
+	assert FDT_contract.balance() == 0
