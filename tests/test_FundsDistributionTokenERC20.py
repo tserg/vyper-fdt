b16401b40a495331db86d8e9d23bd62024ad08c1
Gary Tse <tse.rong.gary@gmail.com>
2021-06-22 20:15:29 +0800

    tests: add test case

--- tests/test_FundsDistributionTokenERC20.py
+++ tests/test_FundsDistributionTokenERC20.py
@@ -86,5 +86,36 @@ def test_different_IO_single_deposit(ERC20, accounts):
 
 	assert tx2.events['FundsDistributed']['receiver'] == accounts[0]
 	assert tx2.events['FundsWithdrawn']['receiver'] == accounts[0]
-	assert ERC20.balanceOf(accounts[0])
+	assert ERC20.balanceOf(fdt_instance) == 0
 	assert ERC20.balanceOf(accounts[0]) == account_balance + 500
+
+def test_two_token_holders_single_deposit(ERC20, accounts):
+
+	"""
+		Two address with 50 tokens each, withdraws
+	"""
+	fdt_instance = FundsDistributionTokenERC20.at(FDT_INSTANCE)
+	tx1 = fdt_instance.transfer(accounts[1], 50, {'from': accounts[0]})
+
+	tx2_1 = ERC20.transfer(accounts[1], 500, {'from': accounts[0]})
+	tx2_2 = ERC20.approve(FundsDistributionTokenERC20[1], 500, {'from': accounts[1]})
+	tx2_3 = FundsDistributionTokenERC20[1].payToContract(500, {'from': accounts[1]})
+
+	assert ERC20.balanceOf(FundsDistributionTokenERC20[1]) == 500
+
+	account_balance = ERC20.balanceOf(accounts[0])
+
+	tx3 = fdt_instance.withdrawFunds({'from': accounts[0]})
+
+	assert tx3.events['FundsDistributed']['receiver'] == accounts[0]
+	assert tx3.events['FundsWithdrawn']['receiver'] == accounts[0]
+	assert ERC20.balanceOf(fdt_instance) == 250
+	assert ERC20.balanceOf(accounts[0]) == account_balance + 250
+
+	account_balance = ERC20.balanceOf(accounts[1])
+
+	tx4 = fdt_instance.withdrawFunds({'from': accounts[1]})
+
+	assert tx4.events['FundsWithdrawn']['receiver'] == accounts[1]
+	assert ERC20.balanceOf(fdt_instance) == 0
+	assert ERC20.balanceOf(accounts[1]) == account_balance + 250
