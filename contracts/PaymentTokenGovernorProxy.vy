e04b5b9ae80e0859e77a77248fda930ec4a0d27c
Gary Tse <tse.rong.gary@gmail.com>
2021-06-30 08:54:01 +0800

    feat: add public getter functions for governor proxy

--- contracts/PaymentTokenGovernorProxy.vy
+++ contracts/PaymentTokenGovernorProxy.vy
@@ -1,16 +1,33 @@
 # @version ^0.2.0
 
+interface PaymentTokenGovernor:
+	def check_payment_token_acceptance(
+		_paymentTokenAddress: address
+	) -> bool: view
+
+	def acceptedPaymentTokenCount() -> uint256: view
+
+	def acceptedPaymentTokens(
+		_index: uint256
+	) -> address: view
+
+	def paymentTokenToIndex(
+		_paymentTokenAddress: address
+	) -> uint256: view
+
 event PaymentTokenGovernorUpdated:
 	oldGovernor: indexed(address)
 	newGovernor: indexed(address)
 
 paymentTokenGovernorAddress: public(address)
+currentPaymentTokenGovernor: PaymentTokenGovernor
 admin: address
 
 @external
 def __init__(_paymentTokenGovernorAddress: address):
 	self.paymentTokenGovernorAddress = _paymentTokenGovernorAddress
 	self.admin = msg.sender
+	self.currentPaymentTokenGovernor = PaymentTokenGovernor(_paymentTokenGovernorAddress)
 
 @external
 def set_payment_token_governor(_address: address):
@@ -23,5 +40,42 @@ def set_payment_token_governor(_address: address):
 	_previousPaymentTokenGovernorAddress: address = self.paymentTokenGovernorAddress
 
 	self.paymentTokenGovernorAddress = _address
+	self.currentPaymentTokenGovernor = PaymentTokenGovernor(_address)
 
 	log PaymentTokenGovernorUpdated(_previousPaymentTokenGovernorAddress, _address)
+
+@external
+@view
+def check_payment_token_acceptance(_paymentTokenAddress: address) -> bool:
+	"""
+	@dev Function to check if payment token is accepted by current PaymentTokenGovernor
+	@return Boolean value indicating True if payment token is accepted. Otherwise, False.
+	"""
+	return self.currentPaymentTokenGovernor.check_payment_token_acceptance(_paymentTokenAddress)
+
+@external
+@view
+def acceptedPaymentTokenCount() -> uint256:
+	"""
+	@dev Get count of accepted payment tokens from current PaymentTokenGovernor
+	@return Integer value for number of payment tokens accepted
+	"""
+	return self.currentPaymentTokenGovernor.acceptedPaymentTokenCount()
+
+@external
+@view
+def acceptedPaymentTokens(_index: uint256) -> address:
+	"""
+	@dev Get accepted payment token by index from current PaymentTokenGovernor
+	@return Address of payment token at given index
+	"""
+	return self.currentPaymentTokenGovernor.acceptedPaymentTokens(_index)
+
+@external
+@view
+def paymentTokenToIndex(_paymentTokenAddress: address) -> uint256:
+	"""
+	@dev Get index of a payment token from current PaymentTokenGovernor
+	@return Given index of payment token in current PaymentTokenGovernor. Returns 0 if it does not exist.
+	"""
+	return self.currentPaymentTokenGovernor.paymentTokenToIndex(_paymentTokenAddress)
---
+++ tests/test_PaymentTokenGovernorProxy.py
@@ -0,0 +1,104 @@
+import pytest
+
+from brownie import (
+	accounts,
+	ERC20,
+	PaymentTokenGovernor,
+	PaymentTokenGovernorProxy,
+)
+
+PAYMENT_TOKEN_NAME = "Sai"
+PAYMENT_TOKEN_SYMBOL = "SAI"
+PAYMENT_TOKEN_DECIMALS = 18
+PAYMENT_TOKEN_SUPPLY = 1000
+
+NEW_PAYMENT_TOKEN_NAME = "Dai"
+NEW_PAYMENT_TOKEN_SYMBOL = "DAI"
+NEW_PAYMENT_TOKEN_DECIMALS = 18
+NEW_PAYMENT_TOKEN_SUPPLY = 1000
+
+ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+@pytest.fixture(scope="module")
+def PaymentToken(ERC20, accounts):
+	yield ERC20.deploy(PAYMENT_TOKEN_NAME, PAYMENT_TOKEN_SYMBOL, PAYMENT_TOKEN_DECIMALS, PAYMENT_TOKEN_SUPPLY, {'from': accounts[0]})
+
+@pytest.fixture(scope="module")
+def NewPaymentToken(ERC20, accounts):
+	yield ERC20.deploy(NEW_PAYMENT_TOKEN_NAME, NEW_PAYMENT_TOKEN_SYMBOL, NEW_PAYMENT_TOKEN_DECIMALS, NEW_PAYMENT_TOKEN_SUPPLY, {'from': accounts[0]})
+
+@pytest.fixture(scope="module", autouse=True)
+def PaymentTokenGovernorContract(PaymentTokenGovernor, accounts):
+	yield PaymentTokenGovernor.deploy({'from': accounts[0]})
+
+@pytest.fixture(scope="module", autouse=True)
+def NewPaymentTokenGovernorContract(PaymentTokenGovernor, accounts):
+	yield PaymentTokenGovernor.deploy({'from': accounts[0]})
+
+@pytest.fixture(scope="module", autouse=True)
+def PaymentTokenGovernorProxyContract(PaymentTokenGovernorContract, accounts):
+	yield PaymentTokenGovernorProxy.deploy(PaymentTokenGovernorContract, {'from': accounts[0]})
+
+@pytest.fixture(autouse=True)
+def isolation(fn_isolation):
+	pass
+
+def test_initial_state(PaymentTokenGovernorContract, accounts):
+
+	assert PaymentTokenGovernorContract.acceptedPaymentTokenCount() == 0
+
+def test_add_payment_token_to_first_governor(PaymentTokenGovernorProxyContract, PaymentTokenGovernorContract, PaymentToken, accounts):
+
+	tx1 = PaymentTokenGovernorContract.add_payment_token(PaymentToken.address, {'from': accounts[0]})
+
+	assert tx1.events['PaymentTokenAdded']['paymentTokenAddress'] == PaymentToken.address
+	assert PaymentTokenGovernorContract.acceptedPaymentTokenCount() == 1
+	assert PaymentTokenGovernorContract.acceptedPaymentTokens(1) == PaymentToken.address
+	assert PaymentTokenGovernorContract.paymentTokenToIndex(PaymentToken.address) == 1
+
+	assert PaymentTokenGovernorProxyContract.acceptedPaymentTokenCount() == 1
+	assert PaymentTokenGovernorProxyContract.acceptedPaymentTokens(1) == PaymentToken.address
+	assert PaymentTokenGovernorProxyContract.paymentTokenToIndex(PaymentToken.address) == 1
+	assert PaymentTokenGovernorProxyContract.check_payment_token_acceptance(PaymentToken.address) == True
+
+'''
+def test_add_two_payment_tokens(PaymentTokenGovernorContract, PaymentToken, NewPaymentToken, accounts):
+
+	tx1 = PaymentTokenGovernorContract.add_payment_token(PaymentToken.address, {'from': accounts[0]})
+
+	assert tx1.events['PaymentTokenAdded']['paymentTokenAddress'] == PaymentToken.address
+	assert PaymentTokenGovernorContract.acceptedPaymentTokenCount() == 1
+	assert PaymentTokenGovernorContract.acceptedPaymentTokens(1) == PaymentToken.address
+	assert PaymentTokenGovernorContract.paymentTokenToIndex(PaymentToken.address) == 1
+
+	tx2 = PaymentTokenGovernorContract.add_payment_token(NewPaymentToken.address, {'from': accounts[0]})
+
+	assert tx2.events['PaymentTokenAdded']['paymentTokenAddress'] == NewPaymentToken.address
+	assert PaymentTokenGovernorContract.acceptedPaymentTokenCount() == 2
+	assert PaymentTokenGovernorContract.acceptedPaymentTokens(2) == NewPaymentToken.address
+	assert PaymentTokenGovernorContract.paymentTokenToIndex(NewPaymentToken.address) == 2
+
+def test_add_two_and_remove_one_payment_token(PaymentTokenGovernorContract, PaymentToken, NewPaymentToken, accounts):
+
+	tx1 = PaymentTokenGovernorContract.add_payment_token(PaymentToken.address, {'from': accounts[0]})
+
+	assert tx1.events['PaymentTokenAdded']['paymentTokenAddress'] == PaymentToken.address
+	assert PaymentTokenGovernorContract.acceptedPaymentTokenCount() == 1
+	assert PaymentTokenGovernorContract.acceptedPaymentTokens(1) == PaymentToken.address
+	assert PaymentTokenGovernorContract.paymentTokenToIndex(PaymentToken.address) == 1
+
+	tx2 = PaymentTokenGovernorContract.add_payment_token(NewPaymentToken.address, {'from': accounts[0]})
+
+	assert tx2.events['PaymentTokenAdded']['paymentTokenAddress'] == NewPaymentToken.address
+	assert PaymentTokenGovernorContract.acceptedPaymentTokenCount() == 2
+	assert PaymentTokenGovernorContract.acceptedPaymentTokens(2) == NewPaymentToken.address
+	assert PaymentTokenGovernorContract.paymentTokenToIndex(NewPaymentToken.address) == 2
+
+	tx3 = PaymentTokenGovernorContract.remove_payment_token(PaymentToken.address, {'from': accounts[0]})
+
+	assert tx3.events['PaymentTokenRemoved']['paymentTokenAddress'] == PaymentToken.address
+	assert PaymentTokenGovernorContract.acceptedPaymentTokenCount() == 1
+	assert PaymentTokenGovernorContract.acceptedPaymentTokens(2) == ZERO_ADDRESS
+	assert PaymentTokenGovernorContract.paymentTokenToIndex(NewPaymentToken.address) == 1
+	assert PaymentTokenGovernorContract.paymentTokenToIndex(PaymentToken.address) == 0
+'''
