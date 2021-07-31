f847952dcb5167d916e26497a70cdaed9f711352
Gary Tse <tse.rong.gary@gmail.com>
2021-07-31 12:28:17 +0800

    update deployment script for erc20 with fee

--- scripts/deploy_erc20_with_fee.py
+++ scripts/deploy_erc20_with_fee.py
@@ -1,6 +1,7 @@
 from brownie import (
 	ERC20,
 	FeeGovernor,
+	FeeGovernorProxy,
 	FundsDistributionTokenERC20WithFee,
 	FundsDistributionTokenERC20WithFeeFactory,
 	accounts,
@@ -11,11 +12,12 @@ def main():
 
 	erc20 = ERC20.deploy("Base Token", "BASE", 18, 200e18, {'from': acct})
 	#print(erc20.address)
-	fg = FeeGovernor.deploy(1e16, {'from': acct})
+	fg = FeeGovernor.deploy(1e8, {'from': acct})
+	fgp = FeeGovernorProxy.deploy(fg.address, {'from': acct})
 
 	fdt_erc20_wf = FundsDistributionTokenERC20WithFee.deploy({'from': acct})
 	fdt_erc20_wf_factory = FundsDistributionTokenERC20WithFeeFactory.deploy(
-		fdt_erc20_wf, acct, erc20.address, fg.address, acct, {'from': acct}
+		fdt_erc20_wf, acct, erc20.address, fgp.address, {'from': acct}
 	)
 	# FundsDistributionToken.deploy('Musicakes', 'MCAKES', 0, 100, {'from': acct})
 
